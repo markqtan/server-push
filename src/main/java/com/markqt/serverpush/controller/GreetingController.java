4a15c57dd8ce3faf2c739dc0fea65ac21b98d9c7
Mark Tan <markqtan@hotmail.com>
2020-09-18 08:07:20 -0700

    init

---
+++ src/main/java/com/markqt/serverpush/controller/GreetingController.java
@@ -0,0 +1,20 @@
+package com.markqt.serverpush.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+import com.markqt.serverpush.model.Greeting;
+import com.markqt.serverpush.model.HelloMessage;
+
+@Controller
+public class GreetingController {
+	@MessageMapping("/hello")
+	@SendTo("/topic/greetings")
+	public Greeting greeting(HelloMessage message) throws Exception {
+		Thread.sleep(1000); // simulated delay
+		return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+	}
+
+}
---
+++ src/test/java/com/markqt/serverpush/ServerPushApplicationTests.java
@@ -0,0 +1,13 @@
+package com.markqt.serverpush;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerPushApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
